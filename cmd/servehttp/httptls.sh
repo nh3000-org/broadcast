d3d8765f459ed55a4d1d4709c1501c0e353126e4
nh3000 <historicanimations@gmail.com>
2025-08-01 20:53:18 -0500

    http

--- cmd/servehttp/httptls.sh
+++ cmd/servehttp/httptls.sh
@@ -1,3 +1,4 @@
-openssl genrsa -out server.key 2048
-openssl ecparam -genkey -name secp384r1 -out server.key
-openssl req -new -x509 -sha256 -key server.key -out server.crt -days 3650
+#openssl genrsa -out server.key 2048
+#openssl ecparam -genkey -name secp384r1 -out server.key
+#openssl req -new -x509 -sha256 -key server.key -out server.crt -days 3650
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout server.pem -out cert.pem -config ssl.conf
