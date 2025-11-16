f70deb605473e8292200c2e15002900cec2d0bc5
nh3000 <historicanimations@gmail.com>
2025-11-15 18:40:03 -0600

    terminal

--- cmd/terminal/nhterminal.go
+++ cmd/terminal/nhterminal.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -10,6 +11,7 @@ import (
 	"time"
 
 	"github.com/gdamore/tcell/v2"
+	"github.com/nats-io/nats.go/jetstream"
 	"github.com/nh3000-org/broadcast/config"
 	"github.com/rivo/tview"
 	"github.com/shirou/gopsutil/v4/cpu"
@@ -78,11 +80,17 @@ func domemory() tview.Table {
 
 	table.SetCell(7, 0, tview.NewTableCell("PGM Used").SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignLeft))
 
-	table.SetCell(9, 0, tview.NewTableCell("Disk /").SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignLeft))
+	table.SetCell(9, 0, tview.NewTableCell("/").SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignLeft))
+	table.SetCell(10, 0, tview.NewTableCell("Used /").SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignLeft))
+	table.SetCell(11, 0, tview.NewTableCell("Free /").SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignLeft))
 
-	table.SetCell(11, 0, tview.NewTableCell("Disk /home").SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignLeft))
+	table.SetCell(13, 0, tview.NewTableCell("/home").SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignLeft))
+	table.SetCell(14, 0, tview.NewTableCell("Used /home").SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignLeft))
+	table.SetCell(15, 0, tview.NewTableCell("Free /home").SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignLeft))
 
-	table.SetCell(13, 0, tview.NewTableCell("Disk /opt").SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignLeft))
+	table.SetCell(17, 0, tview.NewTableCell("/opt").SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignLeft))
+	table.SetCell(18, 0, tview.NewTableCell("Used /opt").SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignLeft))
+	table.SetCell(19, 0, tview.NewTableCell("Free /opt").SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignLeft))
 	// values
 	table.SetCell(0, 1, tview.NewTableCell("Value").SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignLeft))
 	table.SetCell(1, 1, tview.NewTableCell(vtotal).SetTextColor(tcell.ColorGreen).SetAlign(tview.AlignLeft))
@@ -94,10 +102,16 @@ func domemory() tview.Table {
 	table.SetCell(7, 1, tview.NewTableCell(strconv.FormatUint(memoryStats.Alloc/1024, 10)).SetTextColor(tcell.ColorGreen).SetAlign(tview.AlignLeft))
 
 	table.SetCell(9, 1, tview.NewTableCell(strconv.FormatUint(usage.Total/1024/1024, 10)).SetTextColor(tcell.ColorGreen).SetAlign(tview.AlignLeft))
+	table.SetCell(10, 1, tview.NewTableCell(strconv.FormatUint(usage.Used/1024/1024, 10)).SetTextColor(tcell.ColorGreen).SetAlign(tview.AlignLeft))
+	table.SetCell(11, 1, tview.NewTableCell(strconv.FormatUint(usage.Free/1024/1024, 10)).SetTextColor(tcell.ColorGreen).SetAlign(tview.AlignLeft))
 
-	table.SetCell(11, 1, tview.NewTableCell(strconv.FormatUint(usagehome.Total/1024/1024, 10)).SetTextColor(tcell.ColorGreen).SetAlign(tview.AlignLeft))
+	table.SetCell(13, 1, tview.NewTableCell(strconv.FormatUint(usagehome.Total/1024/1024, 10)).SetTextColor(tcell.ColorGreen).SetAlign(tview.AlignLeft))
+	table.SetCell(14, 1, tview.NewTableCell(strconv.FormatUint(usagehome.Used/1024/1024, 10)).SetTextColor(tcell.ColorGreen).SetAlign(tview.AlignLeft))
+	table.SetCell(15, 1, tview.NewTableCell(strconv.FormatUint(usagehome.Free/1024/1024, 10)).SetTextColor(tcell.ColorGreen).SetAlign(tview.AlignLeft))
 
-	table.SetCell(13, 1, tview.NewTableCell(strconv.FormatUint(usageopt.Total/1024/1024, 10)).SetTextColor(tcell.ColorGreen).SetAlign(tview.AlignLeft))
+	table.SetCell(17, 1, tview.NewTableCell(strconv.FormatUint(usageopt.Total/1024/1024, 10)).SetTextColor(tcell.ColorGreen).SetAlign(tview.AlignLeft))
+	table.SetCell(18, 1, tview.NewTableCell(strconv.FormatUint(usageopt.Used/1024/1024, 10)).SetTextColor(tcell.ColorGreen).SetAlign(tview.AlignLeft))
+	table.SetCell(19, 1, tview.NewTableCell(strconv.FormatUint(usageopt.Free/1024/1024, 10)).SetTextColor(tcell.ColorGreen).SetAlign(tview.AlignLeft))
 
 	// uom
 	table.SetCell(0, 2, tview.NewTableCell("UOM").SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignLeft))
@@ -110,14 +124,108 @@ func domemory() tview.Table {
 	table.SetCell(7, 2, tview.NewTableCell("k").SetTextColor(tcell.ColorBlue).SetAlign(tview.AlignLeft))
 
 	table.SetCell(9, 2, tview.NewTableCell("gb").SetTextColor(tcell.ColorBlue).SetAlign(tview.AlignLeft))
-
+	table.SetCell(10, 2, tview.NewTableCell("gb").SetTextColor(tcell.ColorBlue).SetAlign(tview.AlignLeft))
 	table.SetCell(11, 2, tview.NewTableCell("gb").SetTextColor(tcell.ColorBlue).SetAlign(tview.AlignLeft))
 
 	table.SetCell(13, 2, tview.NewTableCell("gb").SetTextColor(tcell.ColorBlue).SetAlign(tview.AlignLeft))
+	table.SetCell(14, 2, tview.NewTableCell("gb").SetTextColor(tcell.ColorBlue).SetAlign(tview.AlignLeft))
+	table.SetCell(15, 2, tview.NewTableCell("gb").SetTextColor(tcell.ColorBlue).SetAlign(tview.AlignLeft))
+
+	table.SetCell(17, 2, tview.NewTableCell("gb").SetTextColor(tcell.ColorBlue).SetAlign(tview.AlignLeft))
+	table.SetCell(18, 2, tview.NewTableCell("gb").SetTextColor(tcell.ColorBlue).SetAlign(tview.AlignLeft))
+	table.SetCell(19, 2, tview.NewTableCell("gb").SetTextColor(tcell.ColorBlue).SetAlign(tview.AlignLeft))
 
 	return *table
 }
 
+type DJ struct {
+	Artist              string
+	Album               string
+	Song                string
+	Length              string
+	SchedRow            string
+	SchedDay            string
+	SchedHour           string
+	SchedPosition       string
+	SchedCategory       string
+	SchedSpinsToPlay    string
+	SchedSpinsLefToPlay string
+}
+
+var DJJSON = DJ{}
+
+var ctxmain context.Context
+var ctxmaincan context.CancelFunc
+
+//var a fyne.App
+//var w fyne.Window
+
+// var onairmp3 jetstream.KeyValue
+var errum error
+var mp3msg jetstream.KeyWatcher
+var mp3err error
+var wavmsg jetstream.KeyWatcher
+var waverr error
+var kve jetstream.KeyValueEntry
+
+func drawonair() {
+	ctxmain, ctxmaincan = context.WithCancel(context.Background())
+	if config.NatsBucketType == "mp3" {
+		mp3msg, mp3err = config.NATS.OnAirmp3.Watch(ctxmain, "OnAirmp3")
+		if mp3err != nil {
+			log.Println("ReceiveONAIRMP3", mp3err)
+			config.Send("messages."+"DJ", "Receive On Air mp3 ", "DJ")
+		}
+		for {
+
+			kve = <-mp3msg.Updates()
+			//log.Println("ReceiveONAIRMP3", kve)
+			if kve != nil {
+				errum = json.Unmarshal(kve.Value(), &DJJSON)
+				if errum != nil {
+					log.Println("DJ ReceiveONAIRMP3", errum)
+					config.Send("messages."+"DJ", "DJ Receive On Air mp3 ", errum.Error())
+
+				}
+				runtime.GC()
+				runtime.ReadMemStats(&memoryStats)
+				if w != nil {
+					w.SetTitle("On Air MP3 " + DJJSON.Artist + " - " + DJJSON.Song + " - " + DJJSON.Album + " " + strconv.FormatUint(memoryStats.Alloc/1024/1024, 10) + " Mib")
+				}
+				drawgGui(DJJSON)
+
+			}
+		}
+	}
+	if config.NatsBucketType == "wav" {
+		wavmsg, waverr = config.NATS.OnAirwav.Watch(ctxmain, "OnAirwav")
+		if waverr != nil {
+			log.Println("ReceiveONAIRWAV", waverr)
+			config.Send("messages."+"DJ", "Receive On Air wav ", "DJ")
+		}
+		for {
+
+			kve = <-wavmsg.Updates()
+			//log.Println("ReceiveONAIRMP3", kve)
+			if kve != nil {
+				errum = json.Unmarshal(kve.Value(), &DJJSON)
+				if errum != nil {
+					log.Println("DJ ReceiveONAIRMP3", errum)
+					config.Send("messages."+"DJ", "DJ Receive On Air wav ", errum.Error())
+
+				}
+				runtime.GC()
+				runtime.ReadMemStats(&memoryStats)
+				if w != nil {
+					w.SetTitle("On Air WAV " + DJJSON.Artist + " - " + DJJSON.Song + " - " + DJJSON.Album + " " + strconv.FormatUint(memoryStats.Alloc/1024/1024, 10) + " Mib")
+				}
+				drawgGui(DJJSON)
+
+			}
+		}
+	}
+
+}
 func drawgui(app *tview.Application, flex *tview.Flex) {
 	runtime.GC()
 	readPreferences()
