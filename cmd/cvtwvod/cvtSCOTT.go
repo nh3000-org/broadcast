9cb62ddc79cefb813b675e178fc5e5219a4062b1
nh3000 <historicanimations@gmail.com>
2026-03-07 17:39:08 -0600

    scott

--- cmd/cvtwvod/cvtSCOTT.go
+++ cmd/cvtwvod/cvtSCOTT.go
@@ -30,15 +30,17 @@ var finxexfileerr error
 var fb = make([]byte, 179)
 var fbindex int64
 var continuereading = true
+var count = 1
 
 func readPath(startpath, station string) {
 
 	os.Chdir(startpath)
-	fbindex = 0
-	walkfileerr := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+	fbindex = 1
+	walkfileerr := filepath.Walk(startpath, func(path string, info os.FileInfo, err error) error {
 		category = ""
 		continuereading = true
 		if info.IsDir() {
+			log.Println("read", info.Name())
 			// determine the category
 			if slices.Contains(musicIncludes, info.Name()) {
 				category = "RECURRENTS"
@@ -56,17 +58,19 @@ func readPath(startpath, station string) {
 		// read the FINDEX01.DAT file block size 179
 
 		if !info.IsDir() && category != "" {
+			log.Println("read2")
 			findexfile, findexfilerror := os.Open("FINDEX01.DAT")
 			if findexfilerror != nil {
 				log.Println("findexfile error reading", findexfilerror)
 			}
 			var seekstart int64 = fbindex * 179
-			_, fbseelerror := findexfile.Seek(seekstart, io.SeekStart)
+			_, fbseelerror := findexfile.Seek(seekstart, 1)
 			if fbseelerror != nil {
 				log.Println("findexfile seek error ", fbseelerror)
 			}
 
-			_, readerr := findexfile.Read(fb)
+			rb, readerr := findexfile.Read(fb)
+			log.Println("read", rb)
 			if readerr != nil {
 				log.Println("findexfile read error ", readerr)
 			}
@@ -85,9 +89,13 @@ func readPath(startpath, station string) {
 			ir.Junk = sfb[46:58]
 			ir.Artist = sfb[59:91]
 			ir.Junk2 = sfb[92:104]
-			ir.File = sfb[105:183]
-			log.Println(ir.Song, ir.Junk, ir.Artist, ir.Junk2, ir.File)
-
+			ir.File = sfb[105:179]
+			log.Println(fb, ir.Song, ir.Junk, ir.Artist, ir.Junk2, ir.File)
+			count++
+			fbindex++
+			if count > 3 {
+				os.Exit(0)
+			}
 		}
 
 		return nil
@@ -101,6 +109,7 @@ func main() {
 	stationid := flag.String("stationid", "WVOD", "-stationid call letters of station")
 
 	flag.Parse()
+	log.Println("init", *rootImport, *stationid)
 	readPath(*rootImport, *stationid)
 	// read the findex
 	// get the data
--- cmd/cvtwvod/cvtSCOTT.sh
+++ cmd/cvtwvod/cvtSCOTT.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd /opt/src/github.com/nh3000/broadcast/cmd/onair
-go build cvtscott.go
-./cvtscott -rootimport "/media/oem/Backup Plus" -stationid WVOD 
+cd /opt/src/github.com/nh3000/broadcast/cmd/cvtwvod
+go build cvtSCOTT.go
+./cvtSCOTT -rootimport "/media/oem/Backup Plus" -stationid WVOD 
