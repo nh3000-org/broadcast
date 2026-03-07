fb88aed79de68c633cdcf70f565c4300a03c86e9
nh3000 <historicanimations@gmail.com>
2026-03-07 16:27:05 -0600

    scott

--- cmd/cvtwvod/cvtSCOTT.sh
+++ cmd/cvtwvod/cvtSCOTT.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd /opt/src/github.com/nh3000/broadcast/cmd/onair
 go build cvtscott.go
-./cvtscott -rootimport "/media/oem/Backup Plus" -stationid WVOD -schedhour 06 -logging true
+./cvtscott -rootimport "/media/oem/Backup Plus" -stationid WVOD 
